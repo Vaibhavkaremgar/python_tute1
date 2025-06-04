0240b6b15b335560014192d9abde0e7cf104c954
unknown <karemgarvaibhav@gmail.com>
2025-06-04 12:00:57 +0530

    Add Task 1 and Task 2 Python scripts

---
+++ task1.py
@@ -0,0 +1,14 @@
+a = int(input("enter a number :- "))
+b = int(input("enter 2nd number :-"))
+
+add = a + b 
+print("the sum of the 2 num is :-", add)
+
+sub = a - b
+print("the diff of 2 numbers is :- ", sub)
+
+mul = a * b
+print("the product of the 2 num is :- ", mul)
+
+division = a / b
+print("the div of 2 num is :- ", division)
---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name :- ")
+last_name = input("enter your last name :- ")
+
+print(f"hello, {first_name} {last_name} ! welcome to the world of python")
