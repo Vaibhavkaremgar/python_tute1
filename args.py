e7dc00b9df0d2c4e6f38596c7a18bbaed4210687
unknown <karemgarvaibhav@gmail.com>
2025-06-24 10:30:14 +0530

    code of 23rd june

--- args.py
+++ args.py
@@ -1,5 +1,14 @@
-def myfunc(*args):
-    for i in args:
-        if i % 2 == 0:
-            return[i]
-myfunc(1,2,3,4,5)
+# def myfunc(*args):
+#     for i in args:
+#         if i % 2 == 0:
+#             return[i]
+# myfunc(1,2,3,4,5)
+
+print("Hello World, I am ready to learn Python with Harry")
+
+name = "Alice"
+age = 25
+height = 5.6
+
+print(f"my name is {name}, and i am {height} feets height with the age of {age}")
+print("hello world!\n  how are \t  \'you \" doing? good \\bad ")
