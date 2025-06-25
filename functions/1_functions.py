e87d871f96dbba990ee377644dc43df53a8d0b70
unknown <karemgarvaibhav@gmail.com>
2025-06-25 11:15:34 +0530

    functions

--- functions/1_functions.py
+++ functions/1_functions.py
@@ -1,14 +1,27 @@
-# def total_products(a,b,c=5):
+# def total_products(a,b,c=5):# default arguments and parameters
 #     return a + b + c 
 # result = total_products(4,6,7)
 # print(result)
 
-# def marriage(groom,bride):
+# def marriage(groom,bride): # keyword arguments
 #     print(f"{groom} and {bride} are getting married")
 # marriage(groom="gopi", bride="ruchita")
 
-def fib(n):
-    if (n == 0  or n == 1):
-        return n
-    return fib(n-2) + fib(n-1)
-print(fib(50))
+
+# square = lambda x: x * x
+# print(square(5))
+
+# tip = lambda bill: bill * 0.10
+# print(tip(500))
+
+# def fib(n): 
+#     if (n == 0  or n == 1):
+#         return n
+#     return fib(n-2) + fib(n-1)
+# print(fib(50))
+
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    return n * factorial(n-1)
+print(factorial(0))
